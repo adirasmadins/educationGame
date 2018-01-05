b4263ba6b01f26be80db02bc775b4e19863081fa
bibibibili <33739008+bibibibili@users.noreply.github.com>
2018-01-05 14:15:47 +0700

    Create menu

---
+++ educationGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void Play (int i)
+	{
+		switch (i) {
+		case 1:
+			{
+				SceneManager.LoadScene ("Game");
+				break;
+			}
+		case 2:
+			{
+				SceneManager.LoadScene ("Game2");
+				break;
+
+			}
+		case 3:
+			{
+				Application.Quit ();
+				break;
+			}
+		default:
+			{
+				break;
+			}
+		}
+		
+	}
+}
