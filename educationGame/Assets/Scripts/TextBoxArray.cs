3b46fc345161d93b029ed3900c23cc36a05e9999
bibibibili <33739008+bibibibili@users.noreply.github.com>
2018-01-15 17:53:19 +0700

    Create button

--- educationGame/Assets/Scripts/TextBoxArray.cs
+++ educationGame/Assets/Scripts/TextBoxArray.cs
@@ -5,25 +5,46 @@ using UnityEngine;
 
 
 public class TextBoxArray : MonoBehaviour {
-
-	private GameObject textBox;
-
+	[SerializeField]
+	private GameObject buttonPrefab;
+	[SerializeField]
 	private Canvas canvas;
-	public int length=5;
-
+	[SerializeField]
+	public int length=14;
+	private string alphabet="abcdefghijklmnopqrstuvwxyz";
 	void Start()
 	{
 		GameObject[] button = new GameObject[length];
-		int position = 0;
-		canvas.transform.position = new Vector3 (0, 0, 0);
-		for (int i = 0; i < length; i++) {
-			
-			button[i] = (GameObject) Instantiate(textBox);
-			button[i].transform.SetParent (canvas.transform);
-			button[i].transform.position = new Vector3 (position+50, -100, 0);
-		}
+		int xposition = -150;
 	
+		canvas.transform.position = new Vector3 (0, 0, 0);
+		for (int i = 0; i < length/2; i++) {
 
+			button[i] = (GameObject) Instantiate(buttonPrefab);
+
+			button[i].transform.SetParent (canvas.transform);
+			button[i].transform.position = new Vector3 (xposition, -200, 0);
+
+
+			button [i].GetComponentsInChildren<Text> () [0].text = alphabet [Random.Range (0, alphabet.Length)].ToString ();
+		
+			xposition = xposition + 50;
+
+		}
+
+		xposition = -150;
+		for (int i = length/2; i <= length; i++) {
+
+			button[i] = (GameObject) Instantiate(buttonPrefab);
+
+			button[i].transform.SetParent (canvas.transform);
+			button[i].transform.position = new Vector3 (xposition, -250, 0);
+		
+
+			button [i].GetComponentInChildren<Text>().text = alphabet [Random.Range (0, alphabet.Length)].ToString ();
+			xposition = xposition + 50;
+
+		}
 	}
 
 	// Update is called once per frame
