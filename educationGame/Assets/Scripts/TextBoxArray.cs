9451232956a997b27ed40db09147f02b7cda51ad
bibibibili <33739008+bibibibili@users.noreply.github.com>
2018-01-17 10:36:27 +0700

    Add random  char and fill box

--- educationGame/Assets/Scripts/TextBoxArray.cs
+++ educationGame/Assets/Scripts/TextBoxArray.cs
@@ -6,19 +6,42 @@ using UnityEngine;
 
 public class TextBoxArray : MonoBehaviour {
 	[SerializeField]
+	private GameObject fillBox;
+	[SerializeField]
 	private GameObject buttonPrefab;
 	[SerializeField]
 	private Canvas canvas;
 	[SerializeField]
 	public int length=14;
 	private string alphabet="abcdefghijklmnopqrstuvwxyz";
+	public 	string test = "Doggy";
 	void Start()
 	{
+		canvas.transform.position = new Vector3 (0, 0, 0);
+		//Text box
+		GameObject[] box = new GameObject[length];
+		int xposition2 = -50;
+
+	
+		for (int i = 0; i < 3; i++) {
+
+			box [i] = (GameObject)Instantiate (fillBox);
+			box [i].transform.SetParent (canvas.transform);
+			box [i].transform.position = new Vector3 (xposition2, -50, 0);
+
+
+
+
+			xposition2 = xposition2 + 50;
+		}
+
+		//Button
+
 		GameObject[] button = new GameObject[length];
 		int xposition = -150;
 	
-		canvas.transform.position = new Vector3 (0, 0, 0);
-		for (int i = 0; i < length/2; i++) {
+
+		for (int i = 0; i < 7; i++) {
 
 			button[i] = (GameObject) Instantiate(buttonPrefab);
 
@@ -33,7 +56,7 @@ public class TextBoxArray : MonoBehaviour {
 		}
 
 		xposition = -150;
-		for (int i = length/2; i <= length; i++) {
+		for (int i = 7; i <14; i++) {
 
 			button[i] = (GameObject) Instantiate(buttonPrefab);
 
@@ -45,10 +68,28 @@ public class TextBoxArray : MonoBehaviour {
 			xposition = xposition + 50;
 
 		}
+
+		//Random  character
+		int randomNumb=0;
+		List<int> loopNumb = new List<int> ();
+		for (int i = 0; i < test.Length; i++) {
+			randomNumb = Random.Range (0, 13);
+			while (!isUnique (randomNumb, loopNumb)) {
+				randomNumb = Random.Range (0, 13);
+			}
+			button [randomNumb].GetComponentInChildren<Text> ().text = test [i].ToString ();
+			Debug.Log (button [randomNumb].GetComponentInChildren<Text> ().text);
+			loopNumb.Add (randomNumb);
+		}
 	}
 
-	// Update is called once per frame
-	/*void Update () {
-		
-	}*/
-}
+	bool isUnique(int numb, List<int> lst){
+		if (lst.Contains (numb))
+			return false;
+		else
+			return true;
+	}
+
+
+
+};
